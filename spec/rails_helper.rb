8aae6ec394daf7ebf1c325b590c37ce3ea390f96
Klaudia Chudy <klaudiachudy94@gmail.com>
2019-12-01 00:09:40 +0100

    Fix specs configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,8 @@ require File.expand_path('../config/environment', __dir__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'support/factory_bot'
+require 'rspec_api_documentation/dsl'
+require 'shoulda/matchers'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -18,4 +19,17 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:all) do
+    FactoryBot.reload
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
