b86a4df0b20ac47ccc09e5faa331c4f369d0e90a
Klaudia Chudy <klaudiachudy94@gmail.com>
2019-12-01 22:46:50 +0100

    Don't use depracated method for enum validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe User do
-  it { is_expected.to define_enum_for(:role).with(worker: 1, student: 0) }
+  it { is_expected.to define_enum_for(:role).with_values(worker: 1, student: 0) }
 
   describe 'validations' do
     let!(:user) { create(:user) }
