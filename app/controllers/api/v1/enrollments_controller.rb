90a5129f7c2220b9a1aa1570f674a102f8a7b70f
Klaudia Chudy <klaudiachudy94@gmail.com>
2019-12-03 09:31:37 +0100

    Edit EnrollmentsController

--- app/controllers/api/v1/enrollments_controller.rb
+++ app/controllers/api/v1/enrollments_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class EnrollmentsController < BaseController
       def create
-        if Enrollments::Create.call(enrollment_params[:course_id], enrollment_params[:user_id])
+        if create_enrollment
           render json: { message: 'User successfully enrolled to the course' }, status: :created
         else
           render json: { error: 'Enrollment user to this course failed' },
@@ -13,7 +13,7 @@ module Api
       end
 
       def destroy
-        if Enrollments::Delete.call(params[:id])
+        if delete_enrollment
           head :no_content
         else
           render json: { error: 'Cannot withdraw user from this course' },
@@ -25,6 +25,14 @@ module Api
 
       def enrollment_params
         params.require(:enrollment).permit(:course_id, :user_id)
+      end
+
+      def create_enrollment
+        Enrollments::Create.call(enrollment_params[:course_id], enrollment_params[:user_id])
+      end
+
+      def delete_enrollment
+        Enrollments::Delete.call(params[:id])
       end
     end
   end
